0d6b184d686ca85524a24767cfb9584f60b28f09
Lu Lu Zhu <Laura@Lus-MacBook-Pro.local>
2016-07-20 21:07:22 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('.menu li').hide();
+    $('.menu svg').click(function () {
+        var didRotate = (this.style.transform == "rotate(90deg)");
+        var rotateValue = (didRotate) ? '0deg' : '90deg';
+
+        $(this).transition({
+            rotate: rotateValue
+        });
+
+        if (didRotate) {
+            $('.menu-svg').css('stroke', '#95989A');
+            $('.menu rect').css('fill', '#95989A');
+            $('.menu li').hide(500, 'linear');
+        } else {
+            $('.menu-svg').css('stroke', '#000');
+            $('.menu rect').css('fill', '#000');
+            $('.menu li').show(500, 'linear');
+        }
+    });
+
+    $('.menu span').hover(function () {
+        $(this).css('color', '#000');
+    }, function () {
+        $(this).css('color', '#95989A');
+    });
+
+    $('.img-content').hide();
+
+
+    $('.img-item').hover(function () {
+        $(this).siblings().show();
+        $(this).css('opacity', '0.8');
+    }, function () {
+        $(this).siblings().hide();
+        $(this).css('opacity', '1');
+    })
+});
